919f6eb97bd8ff7d0c4dc3d1d444073d3958ee89
BugayAndrey <testert287@gmail.com>
2015-11-30 19:08:00 +0200

    1.5 Prepared homework testin git commits

--- src/test/java/MyPackage1/MyFirstTest.java
+++ src/test/java/MyPackage1/MyFirstTest.java
@@ -11,30 +11,30 @@ import java.io.File;
 
 public class MyFirstTest {
 	Path Dir = null; //temp directory adress
-	File file = null; 
+	File file = null;
 	boolean created = false; // true - if file was created 
-	
-	@BeforeClass ( groups = {"positive","negative"})
+
+	@BeforeClass(groups = {"positive", "negative"})
 	public void BeforeClass() throws IOException {
 		String tmp_dir_prefix = "Name_of_temp_directory";
 		// set a prefix
 		Dir = Files.createTempDirectory(tmp_dir_prefix);
-		System.out.println(Dir.toString());  
+		System.out.println(Dir.toString());
 	}
 
-	@AfterClass  (groups = {"positive","negative"})
-	public void AfterClass() throws IOException {		
-		Files.deleteIfExists(Dir);		//remove temp directory
+	@AfterClass(groups = {"positive", "negative"})
+	public void AfterClass() throws IOException {
+		Files.deleteIfExists(Dir);        //remove temp directory
 	}
 
-	@AfterMethod  (groups = {"positive","negative"})
+	@AfterMethod(groups = {"positive", "negative"})
 	public void RemoveFile() {
-		if (created) {		// if file was created
+		if (created) {        // if file was created
 			file.delete();  // than delete it
-			}
-		} 	
+		}
+	}
 
-	@Test ( groups = {"positive"})
+	@Test(groups = {"positive"})
 	public void FirstTest() throws IOException {
 		created = false;
 		file = new File(Dir.toString() + '\\' + "test.txt");
@@ -42,7 +42,7 @@ public class MyFirstTest {
 		Assert.assertEquals(created, true);
 	}
 
-	@Test ( groups = {"positive"})
+	@Test(groups = {"positive"})
 	public void SecondTest() throws IOException {
 		created = false;
 		file = new File(Dir.toString() + '\\' + "1234567890.txt");
@@ -50,21 +50,21 @@ public class MyFirstTest {
 		AssertJUnit.assertEquals(true, created);
 	}
 
-	@Test ( groups = {"negative"},alwaysRun = true)
-	public void ThirdTestNegative() throws IOException {	
+	@Test(groups = {"negative"}, alwaysRun = true)
+	public void ThirdTestNegative() throws IOException {
 		created = false;
-		
-		file = new File(Dir.toString() + '\\' + "FileName.txt");		
-			created = file.createNewFile();		//file created (should be deleted after test)
+
+		file = new File(Dir.toString() + '\\' + "FileName.txt");
+		created = file.createNewFile();        //file created (should be deleted after test)
 		try {
-			Assert.assertEquals(file.createNewFile(),false,"The dublicate of file was created"); // creating the same file (result should be false); ! - let avoid useless variable
-		} catch (IOException e) {			
+			Assert.assertEquals(file.createNewFile(), false, "The dublicate of file was created"); // creating the same file (result should be false); ! - let avoid useless variable
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 			
+		}
 	}
 
-	@Test ( groups = {"negative"}, alwaysRun = true)
-	public void FourthTestNegative() throws IOException {	
+	@Test(groups = {"negative"}, alwaysRun = true)
+	public void FourthTestNegative() throws IOException {
 		SoftAssert s = new SoftAssert();
 		created = false;
 		s.assertEquals(created, false, "Variable \"Created\" isn't false");
@@ -78,7 +78,9 @@ public class MyFirstTest {
 			e.printStackTrace();
 		}
 		s.assertAll();
-		
+
 	}
+
+//ukiuqafyuG9DTFHASUISER;IODTRHWQS
 	
 }
